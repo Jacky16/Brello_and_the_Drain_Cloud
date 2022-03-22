d39149e1b4e40dd173fb8a8b8ddc2a6b68c8d041
Venti <diguitys225@gmail.com>
2022-03-22 18:27:55 +0100

    Bug fixeado del salto de offmesh

--- Assets/OffMeshLinkManager.cs
+++ Assets/OffMeshLinkManager.cs
@@ -3,17 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public enum OffMeshLinkMoveMethod
-{
-    Teleport,
-    NormalSpeed,
-    Parabola
-}
 
 public class OffMeshLinkManager : MonoBehaviour
 {
     [SerializeField]
-    private OffMeshLinkMoveMethod method = OffMeshLinkMoveMethod.Parabola;
+    [Range(0,1)]
+    float jumpDuration;
+
+    [SerializeField]
+    [Range(0, 5)]
+    float maxHeightJump;
 
     private Animator animator;
 
@@ -22,37 +21,19 @@ public class OffMeshLinkManager : MonoBehaviour
     {
         NavMeshAgent agent = GetComponent<NavMeshAgent>();
         agent.autoTraverseOffMeshLink = false;
+
         while (true)
         {
             if (agent.isOnOffMeshLink)
             {
-                if (method == OffMeshLinkMoveMethod.NormalSpeed)
-                {
-                    yield return StartCoroutine(NormalSpeed(agent));
-                }
-                else if (method == OffMeshLinkMoveMethod.Parabola)
-                {
-                    yield return StartCoroutine(Parabola(agent, 2.0f, 0.5f));
-                }
-                agent.CompleteOffMeshLink();
+                yield return StartCoroutine(Jump(agent, maxHeightJump, jumpDuration));
             }
+            agent.CompleteOffMeshLink();
             yield return null;
         }
     }
 
-    IEnumerator NormalSpeed(NavMeshAgent agent)
-    {
-        OffMeshLinkData data = agent.currentOffMeshLinkData;
-        Vector3 endPos = data.endPos + Vector3.up * agent.baseOffset;
-
-        while (agent.transform.position != endPos)
-        {
-            agent.transform.position = Vector3.MoveTowards(agent.transform.position, endPos, agent.speed * Time.deltaTime);
-            yield return null;
-        }
-    }
-
-    IEnumerator Parabola(NavMeshAgent agent, float height, float duration)
+    IEnumerator Jump(NavMeshAgent agent, float height, float duration)
     {
         OffMeshLinkData data = agent.currentOffMeshLinkData;
         Vector3 startPos = agent.transform.position;
@@ -61,7 +42,7 @@ public class OffMeshLinkManager : MonoBehaviour
 
         while (normalizedTime < 1.0f)
         {
-            float yOffset = height * 4.0f * (normalizedTime - normalizedTime * normalizedTime);
+            float yOffset = height * (normalizedTime - normalizedTime * normalizedTime);
             agent.transform.position = Vector3.Lerp(startPos, endPos, normalizedTime) + yOffset * Vector3.up;
             normalizedTime += Time.deltaTime / duration;
             yield return null;
