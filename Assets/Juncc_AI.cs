beabaa649c85b3d7f5a2bf50260d80644fe67183
Venti <diguitys225@gmail.com>
2022-04-28 20:04:22 +0200

    Terminado juncc

--- Assets/Juncc_AI.cs
+++ Assets/Juncc_AI.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class Juncc_AI : EnemyAI
 {
@@ -9,10 +10,12 @@ public class Juncc_AI : EnemyAI
     PyraHealth pyraHealth;
     [SerializeField] Transform mudBallCannon;
     bool isOutside;
+    bool isChanging;
     protected override void Start()
     {
         isAttacking = false;
         isOutside = false;
+        isChanging = false;
         player = GameObject.FindGameObjectWithTag("Player");
         pyraHealth = GameObject.FindGameObjectWithTag("Pyra").GetComponent<PyraHealth>();
         combatManager = player.GetComponent<CombatManager>();
@@ -23,54 +26,76 @@ public class Juncc_AI : EnemyAI
     // Update is called once per frame
     protected override void Update()
     {
-        base.Update();
+        if (!playerInSightRange || !pyraHealth.GetIsProtected())
+        {
+            if (combatManager.enemyList.Contains(gameObject))
+            {
+                combatManager.enemyList.Remove(gameObject);
+            }
+        }
+
+        Debug.LogError("canAttack: " + canAttack);
         StateChecker();
     }
 
     protected override void FixedUpdate()
     {
         playerInSightRange = Physics.CheckSphere(transform.position, detectionRadius, playerMask);
-        playerInAttackRange = Physics.CheckSphere(transform.position, attackRadius, playerMask);
-    }
-    protected override void Idle()
-    {
-        
     }
 
     private void StateChecker()
     {
-        if (isOutside && !pyraHealth.GetIsProtected())
+        if ((isOutside && !pyraHealth.GetIsProtected() && !isChanging) || (isOutside && !playerInSightRange))
         {
+            isChanging = true;
+            isOutside = false;
             animator.SetTrigger("Disappear");
         }
-        else if(!isOutside && pyraHealth.GetIsProtected() && playerInSightRange)
+        else if(!isOutside && pyraHealth.GetIsProtected() && playerInSightRange && !isChanging)
         {
+            if (!combatManager.enemyList.Contains(gameObject))
+            {
+                combatManager.enemyList.Add(gameObject);
+            }
+
+            isChanging = true;
+            isOutside = true;
+            transform.DOLookAt(new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z), 0.5f);
             animator.SetTrigger("Appear");
+        }
+        else if(isOutside && pyraHealth.GetIsProtected() && playerInSightRange)
+        {
+            transform.DOLookAt(new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z), 0.5f);
+            AttackPlayer();
         }
     }
     protected override void ChasePlayer() { }
 
-    public void SetAppeared()
+    public void SetChanging()
     {
-        isOutside = true;
+        isChanging = false;
     }
 
-    public void SetDisappeared()
-    {
-        isOutside = false;
-    }
     protected override void AttackPlayer()
     {
-        if(isOutside && canAttack)
+        if  (canAttack)
         {
+            Debug.Log("AttackPlayer");
+            canAttack = false;
             animator.SetTrigger("Attack");
         }
     }
+
+    public void SetCanAttack()
+    {
+        canAttack = true;
+    }
+
     public override void AttackAction()
     {
+        Debug.Log("AttackAction");
         isAttacking = true;
         Instantiate(mudBall, mudBallCannon.position, Quaternion.identity);
-        canAttack = false;
         StartCoroutine(ResetAttack());
     }
 
