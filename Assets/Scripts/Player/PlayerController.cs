15f875661bda228b07dca8af9849f4cb27f92946
Adria Martinez <adriamartinez94@gmail.com>
2022-05-04 16:58:17 +0200

    Actualizado Idle Brello

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -386,7 +386,6 @@ public class PlayerController : MonoBehaviour
 
     private void OnOutSwiming(Collider other)
     {
-        print("Ha salido del agua");
         if (other.CompareTag("Water"))
         {
             currentTorrentDirection = Vector3.zero;
