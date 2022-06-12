771fb38624167a33095b33be142944a1ee8ea81d
Venti <diguitys225@gmail.com>
2022-06-12 18:52:23 +0200

    Intento de frutas + seteada la caja

--- Assets/Scripts/Collectibles/Fruit.cs
+++ Assets/Scripts/Collectibles/Fruit.cs
@@ -4,24 +4,44 @@ using UnityEngine;
 
 public class Fruit : MonoBehaviour
 {
+    [SerializeField] GameObject healParticles;
+    Animator anim;
     Rigidbody rb;
+    public bool canBePickedUp;
     // Start is called before the first frame update
     void Start()
     {
+        anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody>();
+        canBePickedUp = false;
+
+        StartCoroutine(JiggleFruit(Random.Range(0f, 1f)));
     }
 
+
+    IEnumerator JiggleFruit(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        Debug.Log("Entro");
+        anim.SetTrigger("Start");
+    }
     public void Drop()
     {
+        anim.SetTrigger("Drop");
         rb.useGravity = true;
+        canBePickedUp = true;
     }
 
     private void OnCollisionEnter(Collision other)
     {
         if(other.gameObject.TryGetComponent(out BrelloHealth brelloHealth))
         {
-            brelloHealth.DoHeal(1);
-            Destroy(gameObject);
+            if (canBePickedUp)
+            {
+                brelloHealth.DoHeal(1);
+                Instantiate(healParticles, transform.position, Quaternion.identity);
+                Destroy(gameObject);
+            }
         }
     }
 }
